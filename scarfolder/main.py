86e158392dcc470859a98e8bc5993707867f705e
Chang Rebecca Swee Fun <rebecca.swee.fun.chang@intel.com>
2018-06-22 17:00:21 +0800

    Added a scarfolder

---
+++ scarfolder/main.py
@@ -0,0 +1,6 @@
+listap = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser("/media/rebeccas/clear-linux/clearlinux/projects")) for f in fn]
+
+for x in listap:
+  listbb.append(x[28:])
+
+print(*('"{}"'.format(item) for item in listbb), sep=',')
