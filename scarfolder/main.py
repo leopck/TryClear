449ecb7fc69b14222dd8e5234951886fc78f8a00
Stanley Phoong Cheong Kwan <stanley.cheong.kwan.phoong@intel.com>
2018-06-28 13:07:11 +0800

    Reset and added scarfolder with newline and write to html feature

--- scarfolder/main.py
+++ scarfolder/main.py
@@ -1,8 +1,34 @@
 import os
 
-listap = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser("/media/rebeccas/clear-linux/clearlinux/projects")) for f in fn]
+# Change this variable accordingly
+build_dir = "/media/rebeccas/clear-linux/clearlinux/projects"
+filename = "/media/rebeccas/work/10_trygit/3.txt"
+filename2 = "/media/rebeccas/work/10_trygit/output.txt"
 
-for x in listap:
-  listbb.append(x[28:])
+def recursive_folder(softdir):
+    listap = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(softdir)) for f in fn]
+    listbb = []
+    listcc = []
+    for x in listap:
+        tmp = '"' + x[28:] +  '"'
+        listbb.append(tmp)    
+    return (','.join(listbb))
 
-print(*('"{}"'.format(item) for item in listbb), sep=',')
+def parse_newline_escape(filename):
+    fileContents = open(filename,"r").read()
+    tmp = fileContents.replace('\"', '/\"')
+    tmp = tmp.replace('\n', '\\n')
+    # import pdb; pdb.set_trace()
+    # f = open(filename2,"w")
+    # f.write(tmp)
+    # f.close()
+
+def write_into_html(args):
+    #check what number is the next html
+    #write all contents into html and js
+    #1.html, output_info_data_source.js, description_info.js
+
+if __name__ == '__main__':
+    print(recursive_folder(build_dir))
+    
+    # fileContents.close()
